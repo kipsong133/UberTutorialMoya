16f22d15a700183ea31c9dab13ca20527ba0732f
kipsong133 <kipsong133@gmail.com>
2021-02-14 14:55:40 +0900

    위치정보관련내용 수정(iOS14로 변경된부분 코드수정)

--- UberTutorialMoya/Controller/HomeController.swift
+++ UberTutorialMoya/Controller/HomeController.swift
@@ -100,6 +100,8 @@ extension HomeController: CLLocationManagerDelegate {
     }
     
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        // iOS 14로 넘어오면서 매소드가 변경된 부분임.
+        // 기존 : locationManager(_ manager:didChagneAuthorization status:)
         // 앱을 사용중이 아닌 경우에도 허용할 것인지 사용자에게 물어보는 코드
         if manager.authorizationStatus == .authorizedWhenInUse {
             locationManager.requestAlwaysAuthorization()
